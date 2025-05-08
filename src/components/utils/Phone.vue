4f5b683c00f6af5606e20100cba536c76b614f28
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-08 23:42:05 +0100

    Ui Fix(poppins)

--- src/components/utils/Phone.vue
+++ src/components/utils/Phone.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="mockup-phone bottom-4 border-primary w-64 md:w-72 lg:w-80 h-[500px] md:h-[650px] mx-auto scale-90 md:scale-100">
+    <div class="font-poppins mockup-phone bottom-4 border-primary w-64 md:w-72 lg:w-80 h-[500px] md:h-[650px] mx-auto scale-90 md:scale-100">
  
   <div class="mockup-phone-display">
     <div class="mockup-browser border-base-300 border w-72">
   <div class="mockup-browser-toolbar">
-    <div class="input">https://wazoni.com/waitlist</div>
+    <div class="input text-sm">https://wazoni.com</div>
   </div>
   <div class="grid place-content-center border-t border-base-300 h-[555px]">Hello!</div>
 </div>
