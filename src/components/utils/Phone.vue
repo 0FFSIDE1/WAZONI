bfe0f55ab1699f8b0e67272792b1749d0e9e4af5
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-08 09:34:05 +0100

    New Ui FEAT

---
+++ src/components/utils/Phone.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="mockup-phone bottom-4 border-primary w-64 md:w-72 lg:w-80 h-[500px] md:h-[700px] mx-auto scale-90 md:scale-100">
+  <!-- <div class="mockup-phone-camera"></div> -->
+  <div class="mockup-phone-display">
+    <div class="mockup-browser border-base-300 border w-72">
+  <div class="mockup-browser-toolbar">
+    <div class="input">https://lineshop.com</div>
+  </div>
+  <div class="grid place-content-center border-t border-base-300 h-[700px]">Hello!</div>
+</div>
+  </div>
+</div>
+</template>
+<script setup>
+</script>
