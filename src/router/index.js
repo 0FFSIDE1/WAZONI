513b0328e7224bcf9437961f97fa08a267377000
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-08 18:55:40 +0100

    Ui Fix

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   { path: '/', component: () => import('@/views/waitlist/Home.vue'), meta: { keepAlive: false}, },
+  { path: '/waitlist', name:'waitlist', component: () => import('@/views/waitlist/SignUp.vue'), meta: { keepAlive: false}, },
+
 //   { path: '/:category', name:'ProductsByCategory', component: () => import('@/pages/ProductsByCategory.vue'), props: route => ({ category: route.params.category }), meta: { keepAlive: false},  },
-//   { path: '/explore', name:'Explore', component: () => import('@/pages/Explore.vue'), meta: { keepAlive: true}, },
 //   { path: '/product/:id', name: 'ProductDetail', component: () => import('@/pages/SingleProduct.vue'), meta: { keepAlive: false},  },
 //   { path: '/checkout', component: () => import('@/pages/Checkout.vue'), meta: { keepAlive: false},},
 //   { path: '/payment/verify', component: () => import('@/pages/Payment.vue'), meta: { keepAlive: false},},
