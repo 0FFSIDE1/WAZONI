f9641da959cc7de305b3441f6575587d713a8532
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-08 18:55:24 +0100

    Ui Fix

--- src/components/utils/Hamburger.vue
+++ src/components/utils/Hamburger.vue
@@ -1,9 +1,7 @@
 <template>
-  <label class="btn btn-circle swap swap-rotate dropdown-toggle">
-    <input type="checkbox" v-model="isOpen" />
-
-    <!-- Hamburger icon -->
+  <button class="btn btn-circle swap swap-rotate" @click="toggle">
     <svg
+      v-if="!isOpen"
       class="swap-off fill-current"
       xmlns="http://www.w3.org/2000/svg"
       width="32"
@@ -11,10 +9,9 @@
       viewBox="0 0 512 512">
       <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
     </svg>
-
-    <!-- Close icon -->
     <svg
-      class="swap-on fill-current"
+      v-else
+      class="swap-off fill-current"
       xmlns="http://www.w3.org/2000/svg"
       width="32"
       height="32"
@@ -22,14 +19,17 @@
       <polygon
         points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
     </svg>
-  </label>
+  </button>
 </template>
-  
+
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, defineEmits } from 'vue'
+
 const isOpen = ref(false)
 const emit = defineEmits(['toggle'])
 
-watch(isOpen, (val) => emit('toggle', val))
+function toggle() {
+  isOpen.value = !isOpen.value
+  emit('toggle', isOpen.value)
+}
 </script>
-  
