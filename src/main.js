54c40bacb44c8431e094add704002a8c4f90b8ae
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-09 17:31:30 +0100

    axios

--- src/main.js
+++ src/main.js
@@ -5,8 +5,17 @@ import { createPinia } from 'pinia';
 import router from './router';
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 const app = createApp(App)
+app.use(
+    Toast, {
+        transition: 'Vue-Toastification__bounce',
+        maxToasts: 5,
+        newestOnTop: true,
+    }
+);
 const pinia = createPinia();
 AOS.init()
 app.use(pinia);
