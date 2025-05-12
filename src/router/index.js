fd05f8966685dc01901985dfdee8216dfef02c9d
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-12 07:09:03 +0100

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-  { path: '/', component: () => import('@/views/waitlist/Home.vue'), meta: { keepAlive: false}, },
+  { path: '/', component: () => import('@/views/waitlist/Home.vue'), meta: { keepAlive: true}, },
   { path: '/waitlist', name:'waitlist', component: () => import('@/views/waitlist/SignUp.vue'), meta: { keepAlive: false}, },
 
 //   { path: '/:category', name:'ProductsByCategory', component: () => import('@/pages/ProductsByCategory.vue'), props: route => ({ category: route.params.category }), meta: { keepAlive: false},  },
 //   { path: '/product/:id', name: 'ProductDetail', component: () => import('@/pages/SingleProduct.vue'), meta: { keepAlive: false},  },
 //   { path: '/checkout', component: () => import('@/pages/Checkout.vue'), meta: { keepAlive: false},},
 //   { path: '/payment/verify', component: () => import('@/pages/Payment.vue'), meta: { keepAlive: false},},
+
+
+
+// Catch-all route (MUST be last)
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('@/views/Error404.vue'), }
 
 ]
 
