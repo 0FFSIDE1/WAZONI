83d98fde3c24cea426e366e612c8c0b71182a499
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-12 11:57:17 +0100

    light and dark mode

--- src/components/utils/Phone.vue
+++ src/components/utils/Phone.vue
@@ -8,7 +8,7 @@
           <div class="input text-sm">https://wazoni.com</div>
         </div>
         <div class="grid place-content-center border-t border-base-300 h-[555px]">
-          <span ref="typedElement" class="text-center text-lg font-semibold px-4"></span>
+          <span ref="typedElement" class="text-center text-lg font-semibold px-4 text-primary"></span>
         </div>
       </div>
     </div>
