59eb8abd34789c5fee8507eda1d54f1516ddf090
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-16 16:52:43 +0100

    dashboard

---
+++ src/components/dashboardComponents/HeroText.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="my-12 px-6">
+    <div class="flex flex-col md:flex-row  md:join-horizontal items-center gap-6">
+        <div class="avatar avatar-online">
+      <div class="w-16 md:w-24 rounded-full">
+        <img src="https://img.daisyui.com/images/profile/demo/gordon@192.webp" />
+      </div>
+    </div><h1 class="text-2xl font-extrabold">Hello OFFSIDE Ltd</h1> — <span>Here is what is happening in your store today!</span>
+    </div>
+    
+</div>
+</template>
+<script setup>
+</script>
