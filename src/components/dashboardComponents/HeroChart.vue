4b4341e7ccaa76b0b1fcc57b08f1b40843f1da1d
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-16 17:48:22 +0100

    dashboard

---
+++ src/components/dashboardComponents/HeroChart.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+import VChart from 'vue-echarts'
+import { use } from 'echarts/core'
+import { LineChart } from 'echarts/charts'
+import { TitleComponent, TooltipComponent, GridComponent, LegendComponent } from 'echarts/components'
+import { CanvasRenderer } from 'echarts/renderers'
+
+use([
+  LineChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  LegendComponent,
+  CanvasRenderer,
+])
+
+const options = ref({
+  title: { text: 'Montly Sales' },
+  tooltip: {},
+  legend: { data: ['2025'] },
+  xAxis: { data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'] },
+  yAxis: {},
+  series: [
+    { name: '2025', type: 'line', data: [5, 20, 36] }
+  ]
+})
+</script>
+
+<template>
+  <VChart :option="options" style="height: 400px; width: 100%" />
+</template>
