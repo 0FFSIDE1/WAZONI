80cdc835dd2af4cdfb6f30d0b14b58c3a62f7008
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-16 14:03:44 +0100

    dashboard

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="min-h-screen w-full">
+        <NavBar />
+        <main>
+            <slot />
+        </main>
+        <div class="md:hidden">
+            <Dock />
+        </div>
+        
+    </div>
+    <Footer />
+
+</template>
+<script setup>
+import NavBar from '@/components/dashboardComponents/Nav/NavBar.vue';
+
+import Dock from '@/components/dashboardComponents/Dock.vue';
+import Footer from '@/components/Footer.vue'
+</script>
