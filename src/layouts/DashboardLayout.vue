4d0ecd0361075931c98cc794c351f9184fd8da82
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-17 11:59:08 +0100

    dashboard

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -9,7 +9,7 @@
       <NavBar :drawerOpen="drawerOpen" :isDesktop="isDesktop" @toggle="toggleDrawer" />
 
       <!-- Main content -->
-      <main class="p-4 md:pr-8">
+      <main class="md:pr-8">
         <slot />
       </main>
 
