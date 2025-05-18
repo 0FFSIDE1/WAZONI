0fee1f4f224fa8cc325295afb504f68d3f13fc4a
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-18 07:56:39 +0100

    fix

--- src/components/dashboardComponents/Nav/Drawer.vue
+++ src/components/dashboardComponents/Nav/Drawer.vue
@@ -86,7 +86,7 @@ const settingsOpen = ref(false)
             </button>
             <transition name="fade">
               <ul v-if="inventoryOpen" class="pl-6 mt-2 space-y-2 text-sm">
-                <li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <li><router-link to="/vendor/dashboard/products/"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
 </svg>
 Products</router-link></li>
@@ -103,7 +103,7 @@ Products Stats</router-link></li>
           </li>
           <!-- Transactions -->
           <li>
-            <router-link to="#"><BanknotesIcon class="w-6" />Transactions</router-link>
+            <router-link to="/vendor/dashboard/transactions/"><BanknotesIcon class="w-6" />Transactions</router-link>
           </li>
           <!-- Orders -->
           <li>
