f7c7654fea9c9bd27f13951bbc11732d68381692
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-19 09:53:30 +0100

    fix

--- src/components/dashboardComponents/Nav/Drawer.vue
+++ src/components/dashboardComponents/Nav/Drawer.vue
@@ -103,7 +103,7 @@ Products Stats</router-link></li>
           </li>
           <!-- Transactions -->
           <li>
-            <router-link to="/vendor/dashboard/transactions/"><BanknotesIcon class="w-6" />Transactions</router-link>
+            <router-link to="/vendor/dashboard/transactions/"><BanknotesIcon class="w-6" />Sales</router-link>
           </li>
           <!-- Orders -->
           <li>
@@ -121,6 +121,11 @@ Products Stats</router-link></li>
             </button>
             <transition name="fade">
               <ul v-if="deliveryOpen" class="pl-6 mt-2 space-y-2 text-sm">
+                <li><router-link to="/vendor/dashboard/parcels/"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+</svg>
+
+Delivery Management</router-link></li>
                 <li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
 </svg>
