2d1a540bbe33e8c260d964b91cafa0df16e00479
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-20 07:01:18 +0100

    fix

--- src/components/dashboardComponents/homeComponents/HeroText.vue
+++ src/components/dashboardComponents/homeComponents/HeroText.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="relative flex justify-center px-6 bg-accent-content h-46">
+<div class="relative flex justify-center px-6 h-46" style="background-image: url('/src/assets/images/Banner.jpeg');">
     <div class="absolute bottom-0 flex flex-col md:flex-row  md:join-horizontal items-center gap-6">
         <div class="avatar avatar-online">
       <div class="w-46 md:w-46 border-8 border-primary border-t-accent-content rounded-full">
