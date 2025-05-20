7b7905723b0e473dd1cb42195ca4f64adc503380
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-20 06:54:13 +0100

    fix

---
+++ src/components/dashboardComponents/VendorCustomerBarChart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-full max-w-full overflow-x-auto">
+    <Bar :data="chartData" :options="chartOptions" />
+  </div>
+</template>
+<script setup>
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+import { Bar } from 'vue-chartjs';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+const props = defineProps({
+  chartData: Object,
+  chartOptions: Object
+});
+</script>
