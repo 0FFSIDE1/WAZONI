8ea9b6e0c3e7bfe727b0ab7593db09a2db26ad97
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-20 08:10:42 +0100

    fix

--- src/components/dashboardComponents/Nav/Drawer.vue
+++ src/components/dashboardComponents/Nav/Drawer.vue
@@ -158,7 +158,7 @@ Request Delivery<kbd class="kbd kbd-xs">Coming Soon</kbd></router-link></li>
             </button>
             <transition name="fade">
               <ul v-if="settingsOpen" class="pl-6 mt-2 space-y-2 text-sm">
-                <li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <li><router-link to="/vendor/dashboard/profile/"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
 </svg>
  Edit Profile</router-link></li>
