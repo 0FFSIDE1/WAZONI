d2cea82a8dc4c822a9742de97b5283a61b3ae897
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-20 06:54:18 +0100

    fix

--- src/components/dashboardComponents/Nav/Drawer.vue
+++ src/components/dashboardComponents/Nav/Drawer.vue
@@ -69,7 +69,7 @@ const settingsOpen = ref(false)
           </li>
           <li>
             <router-link to="/vendor/dashboard/">
-              <WalletIcon class="w-6" />Wallet    
+              <WalletIcon class="w-6" />Wallet<kbd class="kbd kbd-xs">Coming Soon</kbd>
             </router-link>
           </li>
           
@@ -97,7 +97,7 @@ Add Products</router-link></li> -->
                 <li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
 </svg>
-Products Stats</router-link></li>
+Products Stats <kbd class="kbd kbd-xs">Coming Soon</kbd></router-link></li>
               </ul>
             </transition>
           </li>
@@ -107,7 +107,7 @@ Products Stats</router-link></li>
           </li>
           <!-- Orders -->
           <li>
-            <router-link to="/vendor/dashboard/orders/"><ShoppingBagIcon class="w-6" />Orders</router-link>
+            <router-link to="/vendor/dashboard/orders/"><ShoppingBagIcon class="w-6" />Orders<kbd class="kbd kbd-xs">Coming Soon</kbd></router-link>
           </li>
           <!-- Delivery Dropdown -->
           <li>
@@ -126,23 +126,24 @@ Products Stats</router-link></li>
 </svg>
 
 Delivery Management</router-link></li>
-                <li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
-</svg>
-Request Delivery</router-link></li>
-                <li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+<li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
 </svg>
 Track Parcel</router-link></li>
+                <li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+</svg>
+Request Delivery<kbd class="kbd kbd-xs">Coming Soon</kbd></router-link></li>
+                
               </ul>
             </transition>
           </li>
            <!-- Customers -->
           <li>
-            <router-link to="#"><UserGroupIcon class="w-6" />Customers</router-link>
+            <router-link to="/vendor/dashboard/customers/"><UserGroupIcon class="w-6" />Customers</router-link>
           </li>
           <li>
-            <router-link to="#"><BellAlertIcon class="w-6" />Notifications</router-link>
+            <router-link to="/vendor/dashboard/notifications/"><BellAlertIcon class="w-6" />Notifications</router-link>
           </li>
         
           <!-- Settings Dropdown -->
@@ -164,19 +165,24 @@ Track Parcel</router-link></li>
                 <li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
 </svg>
-Enable 2FA</router-link></li>
-                <li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+Enable 2FA<kbd class="kbd kbd-xs">Coming Soon</kbd></router-link></li>
+                <!-- <li><router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>
-Renew Plan</router-link></li>
+Renew Plan<kbd class="kbd kbd-xs">Coming Soon</kbd></router-link></li> -->
               </ul>
             </transition>
+          </li>
+          <li class="hidden md:block">
+            <router-link to="#"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+      </svg>Support Center</router-link>
           </li>
         </ul>
 
         <!-- Footer Links -->
-        <div class="absolute bottom-12 left-4">
-          <ul class="flex flex-row items-center text-sm gap-8">
+        <div class="absolute bottom-2 left-4 mt-4">
+          <ul class="flex flex-row items-center text-sm justify-around gap-16">
             <li>
               <router-link class="flex items-center gap-2" to="/">
                 <HomeIcon class="w-4" />Website
