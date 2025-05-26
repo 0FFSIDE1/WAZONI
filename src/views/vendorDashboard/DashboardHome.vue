59071e8aa7b9d1c0040b9b53804ab66c932d3167
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-26 19:45:41 +0100

    fix: enhance vendor dashboard by adding conditional rendering for stats and trust score

--- src/views/vendorDashboard/DashboardHome.vue
+++ src/views/vendorDashboard/DashboardHome.vue
@@ -3,17 +3,20 @@
     <!-- HERO Section -->
     <div class="space-y-4">
       <HeroText v-if="vendorStore.info" :brandName="vendorStore.info?.brandName"/>
-      <HeroStat class="w-full" />
+      <HeroStat v-if="vendorStore.stats" :totalSales="vendorStore.stats?.totalSales" :totalOrders="vendorStore.stats?.totalOrders" :totalRevenue="vendorStore.stats?.totalRevenue" :percentageCompletedOrders="vendorStore.stats?.percentageCompletedOrders" class="w-full" />
     </div>
     <div class="flex flex-col justify-center items-center">
       <!-- <h2 class="text-2xl font-semibold text-center mb-4">Rating</h2> -->
-       <TrustScore />
+       <TrustScore v-if="vendorStore.info" :trustScore="vendorStore.info?.trustScore" />
     </div>
 
     <!-- CHARTS Section -->
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-      <HeroChart class="col-span-1 md:col-span-2" />
-      <HeroPieChart class="col-span-1" />
+      <HeroChart v-if="vendorStore.stats"
+                  :monthlySales="vendorStore.stats?.monthlySales"
+                  :monthlyOrders="vendorStore.stats?.monthlyOrders"
+                  class="col-span-1 md:col-span-2" />
+      <HeroPieChart class="col-span-1" v-if="vendorStore.stats" :categoryData="vendorStore.stats?.salesByCategory" />
     </div>
    
      
@@ -23,7 +26,7 @@
     <div class="grid grid-cols-1 md:grid-cols-2 gap-12 items-start">
       <div>
         <h2 class="text-2xl font-semibold text-center mb-4">Recent Orders</h2>
-        <RecentOrder />
+        <RecentOrder :orders="vendorStore.orders.orders" />
       </div>
       <div>
         <h2 class="text-2xl font-semibold text-center mb-4">Customers</h2>
@@ -34,7 +37,7 @@
     <!-- TRANSACTIONS Section -->
     <div class="py-8">
       <h3 class="text-center text-2xl font-semibold mb-6">Recent Transactions</h3>
-      <RecentTransaction />
+      <RecentTransaction :transactions="vendorStore.transactions" />
     </div>
   </section>
 </template>
@@ -50,7 +53,7 @@ import RecentOrder from '@/components/dashboardComponents/homeComponents/RecentO
 import RecentCustomers from '@/components/dashboardComponents/homeComponents/RecentCustomers.vue';
 import RecentTransaction from '@/components/dashboardComponents/homeComponents/RecentTransaction.vue';
 import { useVendorStore } from '@/store/VendorStore';
-import { ref, onMounted } from 'vue';
+import { onMounted } from 'vue';
 
 
 const vendorStore = useVendorStore();
@@ -59,6 +62,8 @@ onMounted(async () => {
   await vendorStore.getVendorInfo();
   await vendorStore.getVendorStats();
   await vendorStore.getVendorOrders();
+  await vendorStore.getVendorNotifications();
+  await vendorStore.getVendorTransactions();
 });
 
 
