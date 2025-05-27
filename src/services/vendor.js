61ddb29a4fc4b52c44e483bce2e797477da20915
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-27 17:01:53 +0100

    fix: remove unused fetchVendorProducts and fetchVendorNotifications functions from vendor service

--- src/services/vendor.js
+++ src/services/vendor.js
@@ -10,11 +10,6 @@ export const fetchVendorStats = async (id) => {
   return data
 }
 
-export const fetchVendorProducts = async () => {
-  const { data } = await api.get('products/')
-  return data
-}
-
 export const fetchVendorOrders = async () => {
   const { data } = await api.get('order/vendor/')
   return data
@@ -27,12 +22,5 @@ export const fetchVendorParcel = async () => {
 
 export const fetchVendorTransactions = async () => {
   const { data } = await api.get('transactions/')
-  return data
-}
-
-export const fetchVendorNotifications = async (url) => {
-  const baseUrl = import.meta.env.VITE_API_URL; 
-  const finalUrl = url ? (url.startsWith('http') ? url : `${baseUrl}${url}`) : `${baseUrl}notifications/`
-  const { data } = await api.get(finalUrl)
   return data
 }
