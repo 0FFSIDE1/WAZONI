d938dc23f39de76e9c5722263b80fe83a57860c8
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-05-30 22:46:08 +0100

    refactor: optimize computed properties for better performance

--- src/components/dashboardComponents/homeComponents/HeroStat.vue
+++ src/components/dashboardComponents/homeComponents/HeroStat.vue
@@ -3,11 +3,12 @@
   <div class="stat">
     <div class="stat-figure text-primary">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
       </svg>
 
+
     </div>
-    <div class="stat-title">Total Sales</div>
+    <div class="stat-title">Sales Revenue</div>
     <div class="stat-value text-primary">₦ {{  formattedTotalSales }}</div>
     <div class="stat-desc">21% more than last month</div>
   </div>
@@ -15,11 +16,12 @@
   <div class="stat">
     <div class="stat-figure text-secondary">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-      </svg>
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+</svg>
+
 
     </div>
-    <div class="stat-title">Total Orders</div>
+    <div class="stat-title">Orders</div>
     <div class="stat-value text-secondary">{{ totalOrders }}</div>
     <div class="stat-desc">21% more than last month</div>
   </div>
@@ -32,19 +34,23 @@
       </svg>
 
     </div>
-    <div class="stat-title">Revenue</div>
+    <div class="stat-title">Orders Revenue</div>
     <div class="stat-value text-secondary">₦ {{ formattedTotalRevenue }}</div>
     <div class="stat-desc">200% more than last month</div>
   </div>
 
   <div class="stat">
     <div class="stat-figure text-secondary">
-  
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m8.99 14.993 6-6m6 3.001c0 1.268-.63 2.39-1.593 3.069a3.746 3.746 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043 3.745 3.745 0 0 1-3.068 1.593c-1.268 0-2.39-.63-3.068-1.593a3.745 3.745 0 0 1-3.296-1.043 3.746 3.746 0 0 1-1.043-3.297 3.746 3.746 0 0 1-1.593-3.068c0-1.268.63-2.39 1.593-3.068a3.746 3.746 0 0 1 1.043-3.297 3.745 3.745 0 0 1 3.296-1.042 3.745 3.745 0 0 1 3.068-1.594c1.268 0 2.39.63 3.068 1.593a3.745 3.745 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.297 3.746 3.746 0 0 1 1.593 3.068ZM9.74 9.743h.008v.007H9.74v-.007Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm4.125 4.5h.008v.008h-.008v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+      </svg>
+
+
     </div>
     <div class="stat-value">{{ percentageCompletedOrders }} %</div>
     <div class="stat-title">Completed Orders</div>
     <div class="stat-desc text-secondary text-[14px]">31 pending orders</div>
-  </div>
+  </div>  
 </div>
 </template>
 <script setup>
