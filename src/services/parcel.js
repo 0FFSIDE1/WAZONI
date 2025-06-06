36e9d41150675f45066e0620e6f31226b0d8ffe9
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-06-06 10:34:45 +0100

    feat: add parcel service with CreateParcel and fetchVendorParcel functions

---
+++ src/services/parcel.js
@@ -0,0 +1,11 @@
+import api from '@/api/axios'
+
+export const CreateParcel = async (payload) => {
+    const { data } = await api.post('parcel/create/', payload)
+    return data
+}
+
+export const fetchVendorParcel = async () => {
+  const { data } = await api.get('parcel/')
+  return data
+}
