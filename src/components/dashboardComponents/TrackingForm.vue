3975a8f86a0e36c77424f203a93ad0ac147cf0f6
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-06-07 12:52:45 +0100

    refactor: simplify TrackingForm component by removing unused code and improving styling

--- src/components/dashboardComponents/TrackingForm.vue
+++ src/components/dashboardComponents/TrackingForm.vue
@@ -1,19 +1,16 @@
 <template>
   <form
     @submit.prevent="trackParcel"
-    class="form-control w-full max-w-md mx-auto"
+    class="form-control w-full max-w-md mx-auto text-center"
     aria-labelledby="tracking-form-title"
   >
-    <label class="label" for="tracking-number">
-      <span class="label-text">Enter Tracking Number</span>
-    </label>
-    <div class="flex space-x-2">
+    <div class="flex justify-center items-center space-x-2 gap-2 flex-col">
       <input
         id="tracking-number"
         v-model="trackingNumber"
         type="text"
         placeholder="Enter your tracking number"
-        class="input input-bordered w-full"
+        class="input input-bordered text-center p-4"
         required
         aria-required="true"
       />
@@ -30,23 +27,13 @@
 </template>
 
 <script setup>
-import { useTrackingStore } from "@/store/TrackingLog";
+import { ref } from 'vue'
+import { useTrackingStore } from '@/store/TrackingLog'
 
-export default {
-  setup() {
-    const store = useTrackingStore();
-    return { store };
-  },
-  data() {
-    return {
-      trackingNumber: "",
-    };
-  },
-  methods: {
-    async trackParcel() {
-      this.store.setTrackingNumber(this.trackingNumber);
-      await this.store.fetchParcelData(this.trackingNumber);
-    },
-  },
-};
+const store = useTrackingStore()
+const trackingNumber = ref('')
+
+const trackParcel = async () => {
+  await store.fetchTrackingInfo(trackingNumber.value)
+}
 </script>
