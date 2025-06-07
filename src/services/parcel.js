76f825315a873b3ce41e5636622d51849d094696
0FFSIDE1 <somtoemmanuel6@gmail.com>
2025-06-07 12:53:10 +0100

    feat: add getTrackingLog function to fetch tracking information by tracking number

--- src/services/parcel.js
+++ src/services/parcel.js
@@ -9,3 +9,8 @@ export const fetchVendorParcel = async () => {
   const { data } = await api.get('parcel/')
   return data
 }
+
+export const getTrackingLog = async (trackingNo) => {
+  const { data } = await api.get(`parcel/${trackingNo}/`)
+  return data
+}
